8accae35fb38a9ba8056ba0526bb49ef21fa58d0
junlanli <lijunlan888@gmail.com>
2017-02-08 16:20:56 +0800

    add refresh model of the spider update message python script

---
+++ refresh_spider_update_message.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from pymongo import MongoClient
+import time
+
+#uri = "mongodb://dba:gHAFE*42@jsNmh@10.10.196.7:27017/admin"
+#client = MongoClient(uri)
+client = MongoClient('10.10.196.7', 27017)
+
+db_auth = client.admin
+db_auth.authenticate("dba", "gHAFE*42@jsNmh")
+
+db = client.mydb
+
+content = db.audit_spider_update_message.find()
+
+def dealDict(dictData):
+	keyList = dictData.keys()
+	for key in keyList:
+		item = dictData[key]
+		if (isinstance(item, (dict))):
+			dealDict(item)
+		elif (isinstance(item, (str, unicode))):
+			newValue = { 'showValue' : item, 'status' : 0}
+			dictData[key] = newValue
+
+
+
+list = []
+
+print time.time()
+
+for item in content :
+	list.append(item)
+
+for item in list :
+	temp = item["old_data"]
+	dealDict(temp)
+	temp = item["new_data"]
+	dealDict(temp)
+
+for item in list :
+	db.for_test.save(item)
+
+print time.time()
+
+#conn = pymongo.Connection("house-be-manage.mongodb",27017)
+#db.authenticate("mongouser","Oq3s9rfS8nkYNO")
+
+#db2 = conn.house_audit
+
+
+
